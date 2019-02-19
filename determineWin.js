0d0f9d20d4b9a6d5c88326f38184f084bb476afd
Darwin Risser <riser.darwin@gmail.com>
2019-02-19 08:45:56 -0600

    working with questions

--- determineWin.js
+++ determineWin.js
@@ -1,11 +1,13 @@
 module.exports = determineWin = (arr) => {
   if (arr[0] === 4){
-    console.log('you made it until season 4!')
+    console.log('you have survived all the way until season 8! Good luck!')
   } else if (arr[0] === 3){
-    console.log('you died in season 3.')
+    console.log('Good job, you died in season 6!')
   } else if (arr[0] === 2){
-    console.log('you died in season 2.')
+    console.log('Not bad, you died in season 4!')
+  } else if (arr[0] === 1){
+    console.log('You died in season 2.')
   } else {
-    console.log('you died in season 1.')
+    console.log('You died in season 1.')
   }
 }
--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const rl = readline.createInterface({
 });
 
 const beginPrompt = () => {
-  rl.question('Choose A, B, C, or D ', (firstAnswer) => {
+  rl.question('If you were the King/Queen of Westeros, who would you choose as your Hand: A. Tyrion Lannister  B. Davos Seaworth C. Tywin Lannister D. Ned Stark ', (firstAnswer) => {
     answerTally(firstAnswer);
     console.log(answers)
-    rl.question('Choose A, B, C, or D ', (secondAnswer) => {
+    rl.question('If you were the King/Queen of Westeros, who would you choose to command your army: A. Robb Stark B. Stannis Baratheon C. Jon Snow D. Ramsay Bolton ', (secondAnswer) => {
       answerTally(secondAnswer);
       console.log(answers)
-      rl.question('Choose A, B, C, or D ', (secondAnswer) => {
+      rl.question('Of the following, who would you LEAST like to fight: A. Gregor Clegane, The Mountain B. Sandor Clegane, The Hound C. Bronn D. Jaime Lannister ', (secondAnswer) => {
         answerTally(secondAnswer);
         console.log(answers)
-        rl.question('Choose A, B, C, or D ', (secondAnswer) => {
+        rl.question('As a citizen of Westeros, Who would you support as the King/Queen: A. Daenerys Targaryen B. Jon Snow C. Joffrey Lannister D. Robert Baratheon ', (secondAnswer) => {
           answerTally(secondAnswer);
           console.log(answers)
           determineWin(answers);
